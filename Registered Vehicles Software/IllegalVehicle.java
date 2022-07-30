eceefdfe3dfaedc3406d8d9ca1a7873b41a59cf4
moezakbar <100241319+moezakbar@users.noreply.github.com>
2022-07-30 17:46:55 -0400

    Add files via upload

---
+++ Registered Vehicles Software/IllegalVehicle.java
@@ -0,0 +1,24 @@
+/*
+Name: Abdul Moez Akbar
+Student number: 20258267
+Date: 29th July'2022
+ */
+package Assignment2;
+
+/**
+ * This is a class that extends the Exception class.
+ * It prints a custom message whenever an IllegalVehicle exception is thrown
+ */
+public class IllegalVehicle extends Exception {
+
+    /**
+     * This is the constructor for this class
+     * @param message a String that represents the message that will be printed
+     */
+    public IllegalVehicle(String message) {
+
+        // calls the print message function of the parent(Exception) class
+        super(message);
+
+    } // end exception
+}
